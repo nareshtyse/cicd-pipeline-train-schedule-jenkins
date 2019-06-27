556b00e1a74c8bbeaad1080ac1844b45eebaa7a9
nareshtyse <48803828+nareshtyse@users.noreply.github.com>
2019-06-27 18:22:00 -0400

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -1,4 +1,4 @@
 export AWS_ACCESS_KEY_ID=AKIAIXE2ERWLRMXK3NGA
 export AWS_SECRET_ACCESS_KEY=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
 export AWS_DEFAULT_REGION=us-east-2
-aws ec2 run-instances --image-id ami-0c55b159cbfafe1f0 --count 1 --instance-type t2.micro --key-name naresh --subnet-id subnet-a25856ca 
+aws ec2 run-instances --image-id ami-01ee8c2cbd5dee779 --count 1 --instance-type t2.micro --key-name naresh --subnet-id subnet-a25856ca 

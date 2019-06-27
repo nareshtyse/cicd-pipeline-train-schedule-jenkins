266d1c23304e795ea35b0a1ab2af898097aaa387
nareshtyse <48803828+nareshtyse@users.noreply.github.com>
2019-06-27 17:16:03 -0400

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -1,1 +1,1 @@
-aws ec2 run-instances --image-id ami-0c55b159cbfafe1f0 --count 1 --instance-type t2.micro --key-name Jenkins --subnet-id subnet-928e2dde
+aws ec2 run-instances --image-id ami-0c55b159cbfafe1f0 --count 1 --instance-type t2.micro --key-name naresh --subnet-id subnet-a25856ca
